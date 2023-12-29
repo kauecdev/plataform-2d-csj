1d5fb9636ec732316541c20e9973a09e92bcf55f
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2023-12-29 16:23:12 -0300

    Created enemy

---
+++ enemies/slime/slime.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = $AnimationTree.get("parameters/playback")
+
+@export var HEALTH = 30
+@export var SPEED = 70
+@export var DAMAGE = 10
+
+var direction = 1
+var is_getting_hit = false
+var is_dead = false
+
+func _ready():
+	animation_tree.active = true
+
+
+func _physics_process(delta):
+	if not is_dead:
+		if not is_getting_hit:
+			state_machine.travel("walk")
+		velocity.x = direction * SPEED
+		move_and_slide()
+		animated_sprite.flip_h = true if direction > 0 else false
+		
+		if is_on_wall():
+			direction = direction * -1
+
+
+func _on_hit_area_body_entered(body):
+	body.on_get_hit(DAMAGE)
+
+
+func on_get_hit(damage):
+	is_getting_hit = true
+	HEALTH -= damage
+	
+	if HEALTH <= 0:
+		state_machine.travel("death")
+	else:
+		state_machine.travel("get_hit")
+	
+
+func on_get_hit_finished():
+	is_getting_hit = false
+	
+	
+func on_death():
+	queue_free()
--- player/Player.gd
+++ player/Player.gd
@@ -1,16 +1,104 @@
 extends CharacterBody2D
 
-@export var speed = 1000
+@onready var player_sprite = $AnimatedSprite2D
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = $AnimationTree.get("parameters/playback")
 
-# Called when the node enters the scene tree for the first time.
+@export var HEALTH = 100
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
+@export var DAMAGE = 10
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var can_double_jump = false
+var is_double_jumping = false
+var is_attacking = false
+var is_getting_hit = false
+var is_dead = false
+var can_attack = true
+
 func _ready():
-	pass
+	animation_tree.active = true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _physics_process(delta):
-	get_input()
+	if not is_dead:
+		jump(delta)
+		move()
+		attack()
 
-func get_input():
-	var input_direction = Input.get_vector("Left", "Up", "Right", "Down")
-	velocity = input_direction * speed
-	print(velocity)
+
+func move():
+	var direction = Input.get_axis("Left", "Right")
+	if direction:
+		player_sprite.flip_h = false if direction > 0 else true
+		velocity.x = direction * SPEED
+		if is_on_floor() and not is_attacking and not is_getting_hit:
+			state_machine.travel("run")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor() and not is_attacking and not is_getting_hit:
+			state_machine.travel("idle")
+			
+	move_and_slide()
+
+
+func jump(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		if not is_double_jumping and not is_attacking and not is_getting_hit:
+			state_machine.travel("jump")
+	else:
+		is_double_jumping = false
+
+	if Input.is_action_just_pressed("Up"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			can_double_jump = true
+		elif can_double_jump:
+			velocity.y = JUMP_VELOCITY
+			can_double_jump = false
+			is_double_jumping = true
+			if not is_attacking:
+				state_machine.travel("double_jump")
+		else:
+			is_double_jumping = false
+
+
+func attack():
+	if Input.is_action_just_pressed("Attack") and can_attack:
+		can_attack = false
+		is_attacking = true
+		is_getting_hit = false
+		state_machine.travel("attack")
+
+
+func on_attack_animation_finished():
+	await get_tree().create_timer(.1).timeout
+	can_attack = true
+	is_attacking = false
+	
+
+func on_get_hit(damage):
+	is_attacking = false
+	is_getting_hit = true
+	HEALTH -= damage
+	
+	if HEALTH <= 0:
+		state_machine.travel("death")
+		is_dead = true
+	else:
+		state_machine.travel("get_hit")
+
+
+func on_get_hit_finished():
+	is_getting_hit = false
+
+
+func on_death_finished():
+	queue_free()
+
+
+func _on_attack_hit_area_body_entered(body):
+	body.on_get_hit(DAMAGE)
