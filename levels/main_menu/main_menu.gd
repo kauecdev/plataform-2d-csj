7344ce0055e0ec8b061353e6157163cb861d4dfd
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-01-09 14:39:14 -0300

    Added logic to hide quit button if it is running on web

--- levels/main_menu/main_menu.gd
+++ levels/main_menu/main_menu.gd
@@ -2,11 +2,15 @@ extends CanvasLayer
 
 @onready var save_and_load = SaveAndLoad
 @onready var continue_button = $VBoxContainer/ContinueButton
+@onready var quit_button = $VBoxContainer/QuitButton
 
 var first_level_path = "res://levels/level_1.tscn"
 
 func _ready():
 	continue_button.disabled = !save_and_load.has_saved_game()
+	if OS.get_name() == "Web":
+		quit_button.visible = false
+		quit_button.disabled = true
 	
 
 func _on_button_pressed():
